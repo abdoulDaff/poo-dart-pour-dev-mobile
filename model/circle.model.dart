1484a588e31feab7dcdf080a93182e851f72ebdb
abdoulDaff <69810054+abdoulDaff@users.noreply.github.com>
2022-06-17 01:41:25 +0000

    Create a circle

--- model/circle.model.dart
+++ model/circle.model.dart
@@ -3,36 +3,31 @@ import 'dart:math';
 import 'point.model.dart';
 import 'shape.model.dart';
 
-
-
 class Circle extends Shape {
-
   Circle({required Point p1, required Point p2}) : super(p1: p1, p2: p2);
 
-  double getRadius(){
-    double radius=p1.distanceTo(p2);
+  double getRadius() {
+    double radius = p1.distanceTo(p2);
     return radius;
   }
 
   @override
   double getArea() {
-    double r=getRadius();
-    return pi*r*r;
+    double r = getRadius();
+    return pi * r * r;
   }
-
 
   @override
   double getPerimeter() {
-    return 2*pi*getRadius();
+    return 2 * pi * getRadius();
   }
 
   @override
   Map<String, dynamic> toJson() {
-   return {
-     'type' : 'Circle',
-     'center':p1.toJson(),
-     'radius':getRadius().toStringAsFixed(2)
-   };
+    return {
+      'type': 'Circle',
+      'center': p1.toJson(),
+      'radius': getRadius().toStringAsFixed(2)
+    };
   }
-
 }
