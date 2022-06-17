3118a62b7fb63dddc74748758951c138679e96f9
abdoulDaff <69810054+abdoulDaff@users.noreply.github.com>
2022-06-17 01:37:48 +0000

    Create Point Class

--- model/point.model.dart
+++ model/point.model.dart
@@ -4,28 +4,31 @@ class Point {
   final double x;
   final double y;
 
-  const Point({required this.x,required this.y});
+  const Point({
+    required this.x,
+    required this.y,
+  });
 
-  Point.fromArray(List<double> data) :
-      x=data[0], y=data[1];
+  Point.fromArray(List<double> data)
+      : x = data[0],
+        y = data[1];
 
-  Point.fromJson(Map<String, dynamic> data) :
-      x=(data['x'] as num).toDouble() , y=(data['y'] as num).toDouble();
+  Point.fromJson(Map<String, dynamic> data)
+      : x = (data['x'] as num).toDouble(),
+        y = (data['y'] as num).toDouble();
 
-  double distanceTo(Point p){
-    double W=x-p.x;
-    double H=y-p.y;
-    return sqrt(W*W+H*H);
+  double distanceTo(Point p) {
+    double W = x - p.x;
+    double H = y - p.y;
+    return sqrt(W * W + H * H);
   }
 
-  Map<String, dynamic> toJson(){
-    return {'x':x,'y':y};
+  Map<String, dynamic> toJson() {
+    return {'x': x, 'y': y};
   }
-
 
   @override
   String toString() {
     return 'Point{x: $x, y: $y}';
   }
-
 }
